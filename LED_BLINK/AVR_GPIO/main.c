ccea0899049b1df931cd2bdd0b755ca88102bc72
Brian Keane <lonesomewhistle@gmail.com>
2020-12-10 08:20:36 -0600

    LED blink

---
+++ LED_BLINK/AVR_GPIO/main.c
@@ -0,0 +1,28 @@
+/*
+ * AVR_GPIO.c
+ *
+ * Created: 12/7/2020 4:23:31 PM
+ * Author : lones
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define F_CPU 16000000UL   // 16 MHz
+#define  LED_ON PORTB |= (1<<PORTB5)
+#define LED_OFF PORTB |= (0<<PORTB5)
+#define TOGGLE_LED PINB |= (1<<PINB5);
+
+int main(void)
+{
+    DDRB |= (1<<DDB5);
+    while (1) 
+    {
+
+		LED_ON;
+		_delay_ms(1000);
+		LED_OFF;
+		//TOGGLE_LED;
+	}
+}
+
